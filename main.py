115e3b4a94b7ec378174e928456f6f3a8fceb374
shivaguru2007-coder <65496645+shivaguru2007-coder@users.noreply.github.com>
2022-02-21 21:15:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,14 +69,14 @@ def start(message):
         user = message.chat.id
         user = str(user)
         sgd = readFile("sg.txt")
-
+        sd = [user]
         msg = message.text
         if msg == '/start':
             if user in sgd:
                 pass
             else:
               with open('sg.txt', 'a', encoding='utf-8') as f:
-               f.write("\n\n".join([user]))
+               f.write("\n\n".join(sd))
  
             print (readFile("sg.txt"))
             print (user)
