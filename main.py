278a9bba51731cb109f005681d95248d76262e83
shivaguru2007-coder <65496645+shivaguru2007-coder@users.noreply.github.com>
2022-02-22 19:26:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -522,14 +522,18 @@ def send_text(message):
         if message.text == "broad1": 
             data = json.load(open("paytmusers.json"))
             bo = readFile("sg.txt")
-            bot.send_message
+            bot.send_message0
 
             for i in bo:
-             info = bo[i]   
+             info = bo[i] 
+             if i == '':
+                 pass
+            else:  
              try:
+                time.sleep(0.8)
                 bot.send_message(info , "none", parse_mode="html")
              except:
-                bot.send_message(OWNER_ID , "user LEaved"+i+""+bo+"", parse_mode="html")
+                bot.send_message(OWNER_ID , "user LEaved "+bo+"", parse_mode="html")
                  
         if message.text == '💰 Balance':
             data = json.load(open('paytmusers.json', 'r'))
