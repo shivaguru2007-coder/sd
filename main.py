c9c6e48f54b833df8bcb5162cd8a10bfd6ead5d8
shivaguru2007-coder <65496645+shivaguru2007-coder@users.noreply.github.com>
2022-02-23 19:18:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -217,9 +217,9 @@ def contact(contact):
                 markups = telebot.types.InlineKeyboardMarkup()
                 markups.add(telebot.types.InlineKeyboardButton(text='✅ Check', callback_data='checkd'))
                 bot.send_message(
-                    user, '🚧 <b>You are invited by <a href="tg://user?id='+str(ref_id)+'">'+str(ref_id)+'</a></b>', parse_mode="html")
+                    user, '🚧 <b>You are invited by <a href="https://tg://user?id='+str(ref_id)+'">'+str(ref_id)+'</a></b>', parse_mode="html")
                 bot.send_message(
-                    ref_id, '🚧 <b>New User On Your Invite Link :  <a href="tg://user?id='+str(user)+'">'+str(user)+'</a>\n💰 +'+str(Per_Refer)+' '+str(TOKEN)+' Added To Your Balance</b>', parse_mode="html")
+                    ref_id, '🚧 <b>New User On Your Invite Link :  <a href="https://tg://user?id='+str(user)+'">'+str(user)+'</a>\n💰 +'+str(Per_Refer)+' '+str(TOKEN)+' Added To Your Balance</b>', parse_mode="html")
                 json.dump(data, open('paytmusers.json', 'w'), indent=4)
                 return menu(contact.from_user.id)
 
